2426bd779aad794dd94177f954ee8cc7d90edb36
Yhonn Condori <yhonncq@gmail.com>
2023-07-26 16:33:18 -0500

    observable mutablelist

--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
@@ -7,19 +7,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
-private fun getWellnessTask() = List(30) { i -> WellnessTask(i, "Task # $i") }
-
 @Composable
 fun WellnessTaskList(
-    modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTask() }
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     LazyColumn(
         modifier = modifier,
         state = rememberLazyListState()
     ) {
-        items(list) { task ->
-            WellnessTaskItem(taskName = task.label)
+        items(
+            items = list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                onClose = { onCloseTask(task) }
+            )
         }
     }
 }
