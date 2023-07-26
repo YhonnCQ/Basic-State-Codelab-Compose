4aa9b1ceff871e2795a1bb32c5a052ba5ea2810b
Yhonn Condori <yhonncq@gmail.com>
2023-07-26 17:04:25 -0500

    state in viewmodel

--- app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTaskList.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTaskList(
     list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     onCloseTask: (WellnessTask) -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -23,6 +23,8 @@ fun WellnessTaskList(
         ) { task ->
             WellnessTaskItem(
                 taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
                 onClose = { onCloseTask(task) }
             )
         }
